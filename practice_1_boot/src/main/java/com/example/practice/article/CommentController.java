951c221ea6a80c4b5a943c940da96f87d056938b
edujeeho <edujeeho@gmail.com>
2024-08-09 14:43:16 +0900

    comments

---
+++ practice_1_boot/src/main/java/com/example/practice/article/CommentController.java
@@ -0,0 +1,33 @@
+package com.example.practice.article;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("articles/{articleId}/comments")
+public class CommentController {
+    private final CommentService service;
+    public CommentController(CommentService service) {
+        this.service = service;
+    }
+
+    // CREATE
+    // Comment는 Article에 종속되어 있으므로 URL 하나만 만들면 된다.
+    // /articles/{articleId}/comments/create
+    @PostMapping("create")
+    public String create(
+            @PathVariable("articleId")
+            Long articleId,
+            @RequestParam("content")
+            String content,
+            @RequestParam("writer")
+            String writer
+    ) {
+        service.create(articleId, content, writer);
+        // POST - redirect - GET
+        return String.format("redirect:/articles/%d", articleId);
+    }
+}
