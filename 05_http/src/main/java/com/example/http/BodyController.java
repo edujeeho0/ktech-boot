49d22edc433d3855edb1717e7e9782618d13d4b7
edujeeho <edujeeho@gmail.com>
2024-08-20 09:55:26 +0900

    complex dtos

--- 05_http/src/main/java/com/example/http/BodyController.java
+++ 05_http/src/main/java/com/example/http/BodyController.java
@@ -1,6 +1,7 @@
 package com.example.http;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +12,7 @@ public class BodyController {
     // @ResponseBody를 추가해준다.
     @PostMapping("/body")
     @ResponseBody
-    public ResponseDto body(
+    public ResponseDto bodyPost(
             // 요청을 JSON 등의 데이터로 받고 싶다면
             // @RequestBody를 추가해준다.
             @RequestBody
@@ -22,5 +23,30 @@ public class BodyController {
         response.setMessage("success");
         response.setStatus(200);
         return response;
+    }
+
+    @GetMapping("/body")
+    @ResponseBody
+    public ArticleDto bodyGet() {
+        ArticleDto article = new ArticleDto();
+        article.setTitle("From Server");
+        article.setContent("Hello World");
+        return article;
+    }
+
+    @GetMapping("/body-str")
+    @ResponseBody
+    public String bodyStr() {
+        return "Hello World!!!";
+    }
+
+    @PostMapping("/body-ac")
+    @ResponseBody
+    public ArticleCommentsDto bodyAc(
+            @RequestBody
+            ArticleCommentsDto dto
+    ) {
+        dto.getComments().add("new comment!!!");
+        return dto;
     }
 }
